dd1e4dbc64da767322d3839428af66de8c04dd0a
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-06-08 18:01:02 +0700

    Update bot_01.php

--- bot_01.php
+++ bot_01.php
@@ -72,7 +72,7 @@ else if($message == "หิวจุง"){
 else if($message == "กี่โมงแล้ว"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "@date";
+        $arrayPostData['messages'][0]['text'] = "ไม่รู้";
         replyMsg($arrayHeader,$arrayPostData);
     }
 else {
