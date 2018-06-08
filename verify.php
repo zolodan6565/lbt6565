45e91ed654ac42f70bf945c6f1cb24b6dde59b1c
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-06-08 15:40:49 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,5 @@
 <?php
-$access_token = 'Vexuc4YGwb4zAIyA6HfOsQGrIWDRWUMTIGxuAAkpCu0q/8zXubJdLlcxyMY06GFuImWCuQtaF2IzXwb4IP8DRlq2eqeApakA8TXK5n6t0mDqc9nwLXykd5x9BL5xLfq8cn6zJbym2Zj8xj47Bn2AwAdB04t89/1O/w1cDnyilFU=
-';
+$access_token = 'TDe3vkudwX2B0LAAHuXzgXqcQcEWLywJbJwJjT+abMoWCiCnwJTv9oeFfTHhSa33ImWCuQtaF2IzXwb4IP8DRlq2eqeApakA8TXK5n6t0mAHg2oa01SeY6Lv1N6B6INUUl8ppXuA5TDR5LW/ObbaiAdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
