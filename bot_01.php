9b61e86e6452c311929bf3a6173465dc233acdf1
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-06-11 14:17:12 +0700

    Update bot_01.php

--- bot_01.php
+++ bot_01.php
@@ -18,6 +18,12 @@
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    else if($message == "ดีจ้า"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
     #ตัวอย่าง Message Type "Sticker"
     else if($message == "ฝันดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
