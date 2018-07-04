683ec9d9e67c0db116ff4aa10966de8ea76e2ebe
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-07-04 13:17:51 +0700

    Update bot_01.php

--- bot_01.php
+++ bot_01.php
@@ -19,13 +19,14 @@
 
 #ตัวอย่าง Message Type "Text"
     if($percent_hello > 60){
-	    for ($i = 0;$i<100;$i++){
+	    
 	$a=array("อืม หวัดดี","สวัสดีจ้าาา","อืม");
         $random_keys=array_rand($a);
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
 	$arrayPostData['messages'][0]['text'] = $a[$random_keys];
-        replyMsg($arrayHeader,$arrayPostData);
+	for ($i = 0;$i<100;$i++){
+        	replyMsg($arrayHeader,$arrayPostData);
     	}
     }
     else if($percent_hello_2 > 60){
