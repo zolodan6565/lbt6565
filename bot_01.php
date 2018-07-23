160602ed694e0682c2d556e996aa541822df3bbd
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-07-23 09:55:17 +0700

    Update bot_01.php

--- bot_01.php
+++ bot_01.php
@@ -1,4 +1,6 @@
 <?php
+$active_chat = "";
+
     date_default_timezone_set("Asia/Bangkok");
     $accessToken = "TDe3vkudwX2B0LAAHuXzgXqcQcEWLywJbJwJjT+abMoWCiCnwJTv9oeFfTHhSa33ImWCuQtaF2IzXwb4IP8DRlq2eqeApakA8TXK5n6t0mAHg2oa01SeY6Lv1N6B6INUUl8ppXuA5TDR5LW/ObbaiAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
   
@@ -12,7 +14,15 @@
    
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
+    if ($message == "คิดถึงบอทคับ"){
+	    $activ_chat = TRUE;
+    }
+    if ($message == "เงียบๆหน่อยคับ"){
+	    $activ_chat = FALSE;
+    }
 
+while ($active_chat == TRUE) {
+	
     $hello = similar_text("สวัสดี","$message",$percent_hello);
     $hello_2 = similar_text("ดีจ้า","$message",$percent_hello_2);
     $what_time = similar_text("กี่โมงแล้ว","$message",$percent_what_time);
@@ -33,6 +43,12 @@
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = $a[$random_keys];
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+    else if ( $message == "คิดถึงบอทคับ"){
+	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "คิดถึงเหมือนกานนนน จุฟ <3";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Sticker"
@@ -124,5 +140,6 @@ function replyMsg($arrayHeader,$arrayPostData){
         $result = curl_exec($ch);
         curl_close ($ch);
     }
+}
    exit;
 ?>
