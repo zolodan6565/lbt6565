6f64fd35890df672eebc92abc6030cb22777124f
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-07-24 11:32:54 +0700

    Update bot_01.php

--- bot_01.php
+++ bot_01.php
@@ -1,7 +1,7 @@
 <?php
-$active_chat = TRUE;
+$active_chat = true;
 
-if ($active_chat == TRUE){
+if ($active_chat == true){
     date_default_timezone_set("Asia/Bangkok");
     $accessToken = "TDe3vkudwX2B0LAAHuXzgXqcQcEWLywJbJwJjT+abMoWCiCnwJTv9oeFfTHhSa33ImWCuQtaF2IzXwb4IP8DRlq2eqeApakA8TXK5n6t0mAHg2oa01SeY6Lv1N6B6INUUl8ppXuA5TDR5LW/ObbaiAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
   
@@ -192,7 +192,7 @@ function replyMsg($arrayHeader,$arrayPostData){
 }
 
 
-if ($active_chat == FALSE){
+if ($active_chat == true){
 	 date_default_timezone_set("Asia/Bangkok");
     $accessToken = "TDe3vkudwX2B0LAAHuXzgXqcQcEWLywJbJwJjT+abMoWCiCnwJTv9oeFfTHhSa33ImWCuQtaF2IzXwb4IP8DRlq2eqeApakA8TXK5n6t0mAHg2oa01SeY6Lv1N6B6INUUl8ppXuA5TDR5LW/ObbaiAdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
   
